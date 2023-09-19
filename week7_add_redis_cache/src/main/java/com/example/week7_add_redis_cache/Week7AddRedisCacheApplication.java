121f06ce65ca8903efe91d0e088bd1ec589992b8
64070103 Wipupat Chomthaworn <64070103@it.kmitl.ac.th>
2023-09-19 09:18:36 +0700

    Create Lab About using Redis(cache in RAM) to week6 Wizard
    Create Lab About using Redis (cache in RAM {keep data before sent to DB}) to week6 Wizard

---
+++ week7_add_redis_cache/src/main/java/com/example/week7_add_redis_cache/Week7AddRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.week7_add_redis_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7AddRedisCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7AddRedisCacheApplication.class, args);
+    }
+
+}
---
+++ week7_add_redis_cache/src/test/java/com/example/week7_add_redis_cache/Week7AddRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week7_add_redis_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7AddRedisCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
