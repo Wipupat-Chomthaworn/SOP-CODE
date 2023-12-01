97c9d0acd172ab945005214989cbc71bc05b60e6
Owen <88475411+Wipupat-Chomthaworn@users.noreply.github.com>
2023-12-01 22:42:32 +0700

    Update UserEventsHandler.java

--- Week11_Saga/core/src/main/java/com/example/core/query/UserEventsHandler.java
+++ Week11_Saga/core/src/main/java/com/example/core/query/UserEventsHandler.java
@@ -7,8 +7,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserEventsHandler {
     @QueryHandler
-    public User findPaymentDeatail(FetchUserPaymentDetailsQuery query){
-        
-    }
+    public User findUserPaymentDetail(FetchUserPaymentDetailsQuery fetchUserPaymentDetailsQuery) {
+        PaymentDetail paymentDetail = PaymentDetail.builder()
+                .cardNumber("123Card")
+                .cvv("123")
+                .name("Meow Meow")
+                .validUntilMonth(12)
+                .validUntilYear(2030)
+                .build();
 
+        User user = User.builder()
+                .firstName("Meow")
+                .lastName("Meow")
+                .userId(fetchUserPaymentDetailsQuery.getUserId())
+                .paymentDetail(paymentDetail)
+                .build();
+
+        return user;
+    }
 }
